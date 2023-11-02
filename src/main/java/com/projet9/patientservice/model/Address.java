db8e2fbc05bbb201109d08ef7ceed0afaa99c3d9
Quentin CAVIN <quentin.cavin@gmail.com>
2023-11-02 17:05:19 +0100

    Ajout classes model

---
+++ src/main/java/com/projet9/patientservice/model/Address.java
@@ -0,0 +1,21 @@
+package com.projet9.patientservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    int id;
+    String number;
+    String street;
+}
---
+++ src/main/java/com/projet9/patientservice/model/Patient.java
@@ -0,0 +1,40 @@
+package com.projet9.patientservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "patient")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+    @ManyToOne
+    @JoinColumn(name="gender_id")
+    private Gender gender;
+
+    @ManyToOne(cascade = CascadeType.ALL) // TODO: Si je supprime une adresse ça sulpprime le patient?
+// https://www.baeldung.com/jpa-cascade-types
+    @JoinColumn(name="address_id")
+    private Address address;
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+
+}

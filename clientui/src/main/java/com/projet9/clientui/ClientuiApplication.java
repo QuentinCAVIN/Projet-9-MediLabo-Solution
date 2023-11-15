5ab405ec25ac2732726914f87e248f45ebb1210c
Quentin CAVIN <quentin.cavin@gmail.com>
2023-11-15 10:54:59 +0100

    Ajout clientui

---
+++ clientui/src/main/java/com/projet9/clientui/ClientuiApplication.java
@@ -0,0 +1,13 @@
+package com.projet9.clientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientuiApplication.class, args);
+	}
+
+}
---
+++ clientui/src/test/java/com/projet9/clientui/ClientuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet9.clientui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

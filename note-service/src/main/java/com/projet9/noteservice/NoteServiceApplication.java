161021f29613332539481f3cd4b7933c4a3825a6
Quentin CAVIN <quentin.cavin@gmail.com>
2023-11-29 08:31:24 +0100

    Ajout microservice note-service

---
+++ note-service/src/main/java/com/projet9/noteservice/NoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projet9.noteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteServiceApplication.class, args);
+	}
+
+}

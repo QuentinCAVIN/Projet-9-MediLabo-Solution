64045921b64d30154a597616b282edd13c868589
Quentin CAVIN <quentin.cavin@gmail.com>
2023-12-05 17:01:25 +0100

    Ajout microservice diabetes-assessment-service

---
+++ diabetes-assessment-service/src/main/java/com/projet9/diabetesassessmentservice/DiabetesAssessmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projet9.diabetesassessmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesAssessmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabetesAssessmentServiceApplication.class, args);
+	}
+
+}
---
+++ diabetes-assessment-service/src/test/java/com/projet9/diabetesassessmentservice/DiabetesAssessmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet9.diabetesassessmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesAssessmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
